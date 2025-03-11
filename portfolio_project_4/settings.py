cdc0f0e84d02ebf30d50b1addbf567a90078ed70
Markus-Hefner <m5zrlnlt@students.codeinstitute.net>
2025-03-11 13:44:09 +0100

    Set SECRET_KEY and DATABASE_URL variables

--- portfolio_project_4/settings.py
+++ portfolio_project_4/settings.py
@@ -25,12 +25,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x=_*jy4kb6mr74g2rwkg0ynyz^uwn0^0orbmv)%=-u3=pookvl'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com',]
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com',]
 
 
 # Application definition
